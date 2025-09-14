54e0e7d4d24ec26c022a2fa56cf8fd7c35d8d6ef
“ruben.garruto <r.garruto@reply.it>
2025-09-15 01:11:30 +0200

    🧹 add missing import

--- backend/src/main/java/com/garruto/portfolio_backend/PortfolioBackendApplication.java
+++ backend/src/main/java/com/garruto/portfolio_backend/PortfolioBackendApplication.java
@@ -1,5 +1,7 @@
 package com.garruto.portfolio_backend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PortfolioBackendApplication {
