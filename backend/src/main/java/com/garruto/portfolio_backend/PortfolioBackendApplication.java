49a354caeb2da2c8d4e39de6b8644e6545aee4ba
“ruben.garruto <r.garruto@reply.it>
2025-09-15 00:39:21 +0200

    Add Spring Boot backend with basic setup

---
+++ backend/src/main/java/com/garruto/portfolio_backend/PortfolioBackendApplication.java
@@ -0,0 +1,11 @@
+package com.garruto.portfolio_backend;
+
+
+@SpringBootApplication
+public class PortfolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBackendApplication.class, args);
+	}
+
+}
