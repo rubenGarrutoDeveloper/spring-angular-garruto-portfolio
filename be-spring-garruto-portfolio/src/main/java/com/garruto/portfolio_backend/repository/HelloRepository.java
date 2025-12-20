dbc64bf1fe519e54ffba35654d7597abef65a67b
“ruben.garruto <r.garruto@reply.it>
2025-12-21 00:32:17 +0100

    🧹 chore: remove hello demo controller, service, repository and entity

--- be-spring-garruto-portfolio/src/main/java/com/garruto/portfolio_backend/repository/HelloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.garruto.portfolio_backend.repository;
-
-import com.garruto.portfolio_backend.entity.Hello;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HelloRepository extends JpaRepository<Hello, Long> {
-}
