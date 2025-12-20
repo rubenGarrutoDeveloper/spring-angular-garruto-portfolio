b68570d23cfeec329fc4238b4abfd57ef0951ca7
“ruben.garruto <r.garruto@reply.it>
2025-12-21 00:35:28 +0100

    🛠️ refactor(controller): simplify TestController

--- be-spring-garruto-portfolio/src/main/java/com/garruto/portfolio_backend/controller/TestController.java
+++ be-spring-garruto-portfolio/src/main/java/com/garruto/portfolio_backend/controller/TestController.java
@@ -1,13 +1,33 @@
 package com.garruto.portfolio_backend.controller;
 
+import com.garruto.portfolio_backend.costant.ApiPaths;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(ApiPaths.BASE_PATH+ApiPaths.TEST_PATH)
+@Log4j2
 public class TestController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "Hello from TestController!";
+    @GetMapping
+    public ResponseEntity<TestDto> test() {
+        TestDto testDto = new TestDto(1L, "test name", "test description");
+        return ResponseEntity.ok(testDto);
     }
+
+    @Data
+    @AllArgsConstructor
+    public static class TestDto {
+        private Long id;
+        private String name;
+        private String description;
+    }
+
 }
---
+++ be-spring-garruto-portfolio/src/main/java/com/garruto/portfolio_backend/costant/ApiPaths.java
@@ -0,0 +1,6 @@
+package com.garruto.portfolio_backend.costant;
+
+public class ApiPaths {
+    public static final String BASE_PATH = "/api/v1";
+    public static final String TEST_PATH ="/test";
+}
